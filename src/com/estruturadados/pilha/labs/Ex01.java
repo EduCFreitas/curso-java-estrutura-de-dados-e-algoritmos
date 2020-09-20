4e76ca75fa4804c9c22b0b490697da86d82b2fd1
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-09-20 12:55:45 -0300

    Ex01 - Pilhas: Empilha e desempilha

---
+++ src/com/estruturadados/pilha/labs/Ex01.java
@@ -0,0 +1,43 @@
+package com.estruturadados.pilha.labs;
+
+import java.util.Scanner;
+
+import com.estruturadados.pilha.Pilha;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		Pilha<Integer> pilha = new Pilha<Integer>();
+		Scanner scan = new Scanner(System.in);
+
+		for(int i=0; i<10; i++) {
+			System.out.println("Informe um número:");
+			int num = scan.nextInt();
+
+			if(num%2==0) {
+				System.out.println("Empilhando o número " + num);
+				pilha.empilha(num);
+			}else {
+				Integer desempilhado = pilha.desempilha();
+
+				if(desempilhado == null) {
+					System.out.println("Pilha está vazia");
+				}else {
+					System.out.println("Número ímpar, desempilhando um elemento da pilha: " + desempilhado);
+				}
+			}
+		}
+
+		System.out.println("Todos os números foram lidos, desempilhando números da pilha:");
+
+		while(!pilha.estaVazia()) {
+			System.out.println("Desempilhando um elemento da pilha: " + pilha.desempilha());
+		}
+
+		System.out.println("Todos os elementos foram desempilhados");
+		scan.close();
+
+	}
+
+}
